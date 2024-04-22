6f54916c506998568d61cd207cb3aa9b6db08018
David Crespo <david.crespo@e-asolutions.com>
2024-04-22 22:43:42 +0100

    add some styling

--- my-workspace/src/app/app.tsx
+++ my-workspace/src/app/app.tsx
@@ -8,22 +8,28 @@ import Home from 'src/components/Home/Home';
 import Menu from 'src/components/Menu/Menu';
 import './app.module.scss';
 import HelloWorld1 from 'src/components/HelloWorld/HelloWorld1/HelloWorld1';
+import Footer from 'src/components/Footer/Footer';
 
 export function App() {
   return (
     <Router>
-      <Menu />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route
-          path="/hello-world"
-          element={
-            <HelloWorld1 alertMessage={'This is an important message.'} />
-          }
-        />
-        <Route path="/list" element={<div>List</div>} />
-        <Route path="*" element={<Navigate to="/" />} />
-      </Routes>
+      <div>
+        <Menu />
+        <div className="main-container">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route
+              path="/hello-world"
+              element={
+                <HelloWorld1 alertMessage={'This is an important message.'} />
+              }
+            />
+            <Route path="/list" element={<div>List</div>} />
+            <Route path="*" element={<Navigate to="/" />} />
+          </Routes>
+        </div>
+        <Footer />
+      </div>
     </Router>
   );
 }
---
+++ my-workspace/src/components/Footer/Footer.tsx
@@ -0,0 +1,20 @@
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <div className="container">
+        <p className="text-center">
+          Created by David Crespo <br />
+          <a
+            href="https://www.linkedin.com/in/davidcrespoarroyo/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            LinkedIn
+          </a>
+        </p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
