26b8fcc24e9b9e28c53ce3728e149408a55b1c94
David Crespo <david.crespo@e-asolutions.com>
2024-04-22 10:56:54 +0100

    improve the e2e test to cover closing the alert message

--- my-workspace/e2e/src/e2e/Alert1.cy.ts
+++ my-workspace/e2e/src/e2e/Alert1.cy.ts
@@ -1,8 +1,10 @@
 describe('e2e', () => {
   beforeEach(() => cy.visit('/'));
 
-  it('should display alert message', () => {
+  it('should open and close alert message', () => {
     cy.get('button').click();
     cy.contains('Warning! This is an important message.');
+    cy.get('button.btn-close').click();
+    cy.contains('Warning! This is an important message.').should('not.exist');
   });
 });
