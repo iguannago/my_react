a53c356f91b3e4c2f10a8cb9b76c71abce051815
David Crespo <david.crespo@e-asolutions.com>
2024-04-22 12:34:35 +0100

    add routers

--- my-workspace/src/app/app.tsx
+++ my-workspace/src/app/app.tsx
@@ -1,11 +1,26 @@
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
 import HelloWorld1 from 'src/components/HelloWorld/HelloWorld1/HelloWorld1';
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 
 export function App() {
   return (
-    <div>
-      <HelloWorld1 alertMessage={'This is an important message.'} />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path="/kumar"
+          element={
+            // <HelloWorld1 alertMessage={'This is an important message.'} />
+            <div>hello</div>
+          }
+        />
+        <Route
+          path="/david"
+          element={
+            <HelloWorld1 alertMessage={'This is an important message.'} />
+          }
+        />
+        <Route path="*" element={<Navigate to="/" />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
