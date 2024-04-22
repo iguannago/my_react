6d29cb8ff56c5d38d6ed40a41717b2c7b291f281
David Crespo <david.crespo@e-asolutions.com>
2024-04-22 22:59:40 +0100

    add more styling

--- my-workspace/src/components/Home/Home.tsx
+++ my-workspace/src/components/Home/Home.tsx
@@ -1,5 +1,45 @@
+import { useEffect, useState } from 'react';
+
 const Home = () => {
-  return <div>Hey you!</div>;
+  const [imageUrl, setImageUrl] = useState('');
+
+  useEffect(() => {
+    const fetchRandomImage = async () => {
+      try {
+        const response = await fetch('https://source.unsplash.com/random');
+        if (!response.ok) {
+          throw new Error('Failed to fetch image');
+        }
+        setImageUrl(response.url);
+      } catch (error) {
+        console.error('Error fetching random image:', error);
+      }
+    };
+
+    fetchRandomImage();
+  }, []);
+
+  return (
+    <div style={{ backgroundImage: `url(${imageUrl})` }}>
+      <div className="content">
+        <h1>This is my Reactjs playground!</h1>
+        <h1>This is my Reactjs playground!</h1>
+        <h1>This is my Reactjs playground!</h1>
+        <h1>This is my Reactjs playground!</h1>
+        <h1>This is my Reactjs playground!</h1>
+        <h1>This is my Reactjs playground!</h1>
+        <h1>This is my Reactjs playground!</h1>
+        <h1>This is my Reactjs playground!</h1>
+        <h1>This is my Reactjs playground!</h1>
+        <h1>This is my Reactjs playground!</h1>
+        <h1>This is my Reactjs playground!</h1>
+        <h1>This is my Reactjs playground!</h1>
+        <h1>This is my Reactjs playground!</h1>
+        <h1>This is my Reactjs playground!</h1>
+        <h1>This is my Reactjs playground!</h1>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
