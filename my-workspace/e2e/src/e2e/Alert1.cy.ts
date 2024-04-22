566029e1ca46b12b6288197e318d182507c15049
David Crespo <david.crespo@e-asolutions.com>
2024-04-22 10:54:33 +0100

    fix e2e test

--- my-workspace/e2e/src/e2e/Alert1.cy.ts
+++ my-workspace/e2e/src/e2e/Alert1.cy.ts
@@ -2,6 +2,7 @@ describe('e2e', () => {
   beforeEach(() => cy.visit('/'));
 
   it('should display alert message', () => {
-    cy.contains('Warning This is an important message.');
+    cy.get('button').click();
+    cy.contains('Warning! This is an important message.');
   });
 });
